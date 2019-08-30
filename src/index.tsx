854f7af4fda1212350b12eb1dcf13bc252e0177b
Konosuke Kachi <github@s.k-kachi.net>
2019-08-30 22:45:28 +0900

    Setup Package

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
 import 'firebase/app';
-import 'firebase/firestore'
+import 'firebase/firestore';
+import React from 'react';
 
 enum DocState {
   UNDEFINED,
@@ -9,106 +9,92 @@ enum DocState {
   UNCONNECTED,
 }
 
-const debugState = (x: DocState, y: DocState, path?: string) => {
-  console.debug(`STATE:\t${DocState[x]}\t->\t${DocState[y]}\t(path=${path})`)
+interface IPathObj<T> {
+  [path: string]: T;
 }
 
-type MyType<T> = { [path: string]: T }
-type DocType = firebase.firestore.DocumentSnapshot
-type FuncType = () => void
-type DocsType = MyType<DocType>
-type FuncsType = MyType<FuncType>
-type DocStatesType = MyType<DocState>
-
-type StateType = { docs: DocsType, funcs: FuncsType, docStates: DocStatesType }
-const initialState = { docs: {}, funcs: {}, docStates: {} }
-
-type ContextType = {
-  subscribe: (path: string) => void,
-  unsubscribe: (path: string) => void,
-  getDoc: (path: string, latest?: boolean) => DocType | undefined,
-  docs: MyType<DocType>
+interface IStateType {
+  docs: IPathObj<firebase.firestore.DocumentSnapshot>;
+  funcs: IPathObj<() => void>;
+  docStates: IPathObj<DocState>;
 }
-const Context = React.createContext<ContextType>({
-  subscribe: _ => { },
-  unsubscribe: _ => { },
+const initialState = { docs: {}, funcs: {}, docStates: {} };
+
+interface IContextType {
+  subscribe: (path: string) => void;
+  unsubscribe: (path: string) => void;
+  getDoc: (path: string, latest?: boolean) => firebase.firestore.DocumentSnapshot | undefined;
+  docs: IPathObj<firebase.firestore.DocumentSnapshot>;
+}
+const Context = React.createContext<IContextType>({
+  docs: {},
   getDoc: _ => undefined,
-  docs: {}
-})
+  subscribe: _ => _,
+  unsubscribe: _ => _,
+});
 
-const Provider: React.FC<React.PropsWithChildren<{ app: firebase.app.App }>> = (props) => {
-  const [state, setState] = React.useState<StateType>(initialState)
+const Provider: React.FC<React.PropsWithChildren<{ app: firebase.app.App }>> = props => {
+  const [state, setState] = React.useState<IStateType>(initialState);
 
   const subscribe = (path: string, once = false) => {
-    const docState = state.docStates[path] || DocState.UNDEFINED
+    const docState = state.docStates[path] || DocState.UNDEFINED;
 
     if (docState === DocState.UNDEFINED || docState === DocState.UNCONNECTED) {
-      console.debug(`onSnapshot (path=${path} state=${DocState[docState]})`)
-      const f = props.app.firestore().doc(path).onSnapshot(snap => {
-        const docStateNow = state.docStates[path]
-        const docStateNext = DocState.CONNECTED
-        const newState: StateType = { ...state }
-        newState.docStates[path] = docStateNext
-        newState.docs[path] = snap
-        debugState(docStateNow, docStateNext, path)
-        setState(newState)
-        if (once) {
-          unsubscribe(path)
-        }
-      }, error => {
-        const docStateNow = state.docStates[path]
-        const docStateNext = DocState.UNDEFINED
-        console.error(error)
-        f()
-        const newState = { ...state }
-        newState.docStates[path] = docStateNext
-        debugState(docStateNow, docStateNext, path)
-        setState(newState)
-      })
-      const docStateNow = docState
-      const docStateNext = DocState.CONNECTING
-      const newState = { ...state }
-      newState.docStates[path] = docStateNext
-      if (newState.funcs[path]) {
-        newState.funcs[path]()
+      const f = props.app
+        .firestore()
+        .doc(path)
+        .onSnapshot(
+          snap => {
+            const newState: IStateType = { ...state };
+            newState.docStates[path] = DocState.CONNECTED;
+            newState.docs[path] = snap;
+            setState(newState);
+            if (once) {
+              unsubscribe(path);
+            }
+          },
+          error => {
+            f();
+            const newState = { ...state };
+            newState.docStates[path] = DocState.UNDEFINED;
+            setState(newState);
+          },
+        );
+      const newStateX = { ...state };
+      newStateX.docStates[path] = DocState.CONNECTING;
+      if (newStateX.funcs[path]) {
+        newStateX.funcs[path]();
       }
-      newState.funcs[path] = f
-      debugState(docStateNow, docStateNext, path)
-      setState(newState)
+      newStateX.funcs[path] = f;
+      setState(newStateX);
     }
-  }
+  };
 
   const unsubscribe = (path: string) => {
-    const docState = state.docStates[path] || DocState.UNDEFINED
+    const docState = state.docStates[path] || DocState.UNDEFINED;
     if (docState === DocState.CONNECTED) {
-      const docStateNow = docState
-      const docStateNext = DocState.UNCONNECTED
-      state.funcs[path]()
-      const newState = { ...state }
-      newState.docStates[path] = docStateNext
-      debugState(docStateNow, docStateNext, path)
-      setState(newState)
+      const docStateNext = DocState.UNCONNECTED;
+      state.funcs[path]();
+      const newState = { ...state };
+      newState.docStates[path] = docStateNext;
+      setState(newState);
     }
-  }
+  };
 
-  const getDoc = (path: string, latest = true): DocType | undefined => {
-    const doc = state.docs[path]
+  const getDoc = (path: string, latest = true): firebase.firestore.DocumentSnapshot | undefined => {
+    const doc = state.docs[path];
     if (doc && !latest) {
-      return doc
+      return doc;
     }
-    subscribe(path, !latest)
-    return doc
-  }
+    subscribe(path, !latest);
+    return doc;
+  };
 
-  const docs = state.docs
+  const docs = state.docs;
 
-  return (
-    <Context.Provider value={{ subscribe, unsubscribe, docs, getDoc }}>
-      {props.children}
-    </Context.Provider>
-  )
-}
+  return <Context.Provider value={{ subscribe, unsubscribe, docs, getDoc }}>{props.children}</Context.Provider>;
+};
 
-export default Provider
-export const useDocumentsContext = () => React.useContext(Context)
-export const DocumentsContext = Context
+export default Provider;
+export const useDocumentsContext = () => React.useContext(Context);
+export const DocumentsContext = Context;
